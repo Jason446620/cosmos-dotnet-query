39396681ab9f76a2506cf050d32e4f4ad32c0e03
Jason446620 <panshubei@outlook.com>
2020-06-19 13:01:00 +0800

    Add project files.

---
+++ CosmosGettingStartedTutorial/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Collections.Generic;
+using System.Net;
+using Microsoft.Azure.Cosmos;
+
+namespace CosmosGettingStartedTutorial
+{
+    class Program
+    {
+        // <Main>
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Beginning operations...\n");
+                CosmosClient client = new CosmosClient("https://localhost:8081/", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==");
+                Database database = await client.CreateDatabaseIfNotExistsAsync("ToDoList");
+                Container container = database.GetContainer("jason");
+                // Query for an item
+                FeedIterator <dynamic> feedIterator =  container.GetItemQueryIterator<dynamic>("SELECT value MIN(c.age) from c");
+                while (feedIterator.HasMoreResults)
+                {
+                    FeedResponse<dynamic> response = await feedIterator.ReadNextAsync();
+                    foreach (var item in response)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+
+            }
+            catch (CosmosException de)
+            {
+                Exception baseException = de.GetBaseException();
+                Console.WriteLine("{0} error occurred: {1}", de.StatusCode, de);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine("End of demo, press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
